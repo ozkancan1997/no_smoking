f65c228d8493f2fceb4969e2c04a3b3d80aa60bc
Özkan Can <ozkan.can@aol.com>
2024-12-22 23:45:10 +0300

    base date changed, since part now uses base date.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const noSmokeCounter = {
     
-    baseDate : new Date(2024, 11, 21, 22, 20, 0, 0),
+    baseDate : new Date(2024, 11, 22, 23, 44, 0, 0),
     
     init : function(){
         const daysLabel = document.getElementById("days");
-
+        const baseDateSpan = document.getElementById("base-date");
+        baseDateSpan.innerText = moment(this.baseDate).format("MMM Do YY");
         setInterval(() => {
             const total = parseInt((Date.now() - this.baseDate.getTime())/86400000);
             daysLabel.innerText = total;
